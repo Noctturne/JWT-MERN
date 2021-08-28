a2403d894a3de5eee81b3fb01ed501687df57352
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-28 12:02:51 +0200

    Hash password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,16 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 exports.createUser = async (req, res) => {
+    const {password} = req.body;
     try {
         let user;
 
         user = new User(req.body);
+
+        const salt = await bcrypt.genSalt(7);
+        user.password = await bcrypt.hash(password, salt);
+
         await user.save();
         res.send('User created!');
         
