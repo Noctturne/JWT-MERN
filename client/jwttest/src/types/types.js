2e134abcf780c0cf20f76f0caa37ea54cf9c68d0
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-28 14:46:04 +0200

    Delete client part

--- client/jwttest/src/types/types.js
+++
@@ -1,7 +0,0 @@
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_ERROR = 'REGISTER_ERROR';
-
-export const GET_USER = 'GET_USER';
-
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_ERROR = 'LOGIN_ERROR';
